f1955744d0be9955dd1794bedcb5cbc4a2c6b2fb
Kalyan Manna <kalyanmanna439@gmail.com>
2026-06-27 00:58:23 +0530

    feat(navbar): add responsive mobile navigation

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -6,6 +6,8 @@ import { useState } from "react";
 import { CaretDown } from "@phosphor-icons/react";
 import { AnimatePresence } from "framer-motion";
 import { MoreMenu } from "./MoreMenu";
+import { Link } from "react-router-dom";
+import { Drawer, DrawerContent, DrawerTrigger } from "../ui/drawer";
 interface NavItem {
   name: string;
   href: string;
@@ -24,13 +26,14 @@ export function NavBar({ items, className }: NavBarProps) {
   return (
     <header
       className={cn(
-        "fixed inset-x-0 top-6 z-50 px-6 md:px-10",
+        "fixed inset-x-0 top-6 z-50 px-4 md:px-10",
         className
       )}
     >
-      <div className="mx-auto grid max-w-7xl grid-cols-[1fr_auto_1fr] items-start gap-4">
+      {/* Desktop */}
+      <div className="hidden md:grid mx-auto max-w-7xl grid-cols-[1fr_auto_1fr] items-start gap-4">
         {/* Logo */}
-        <a href="/" className="flex items-center justify-self-start">
+        <Link to="/" className="flex items-center justify-self-start">
           <img
             src="/logo-white.png"
             alt="Kalyan Manna Logo"
@@ -38,7 +41,7 @@ export function NavBar({ items, className }: NavBarProps) {
             height={55}
             className="object-contain"
           />
-        </a>
+        </Link>
 
         {/* Navbar */}
         <motion.div
@@ -61,44 +64,45 @@ export function NavBar({ items, className }: NavBarProps) {
             shadow-[0_0_30px_rgba(255,255,255,0.08)]
           ">
           <div className="flex items-center justify-center p-1.5">
-            {items.map((item) => {
-              const isActive = !item.hasDropdown && active === item.href;
+            {items
+              .map((item) => {
+                const isActive = !item.hasDropdown && active === item.href;
 
-              return (
-                <a
-                  key={item.href}
-                  href={item.href}
-                  onClick={() => {
-                    if (item.hasDropdown) {
-                      setMoreOpen((prev) => !prev);
-                      return;
-                    }
+                return (
+                  <Link
+                    key={item.href}
+                    to={item.href}
+                    onClick={() => {
+                      if (item.hasDropdown) {
+                        setMoreOpen((prev) => !prev);
+                        return;
+                      }
 
-                    setActive(item.href);
-                  }}
-                  onMouseEnter={() => {
-                    if (item.hasDropdown && !moreOpen) {
-                      setMoreOpen(true);
-                    }
-                  }}
-                  className={cn(
-                    "relative flex items-center justify-center rounded-[30px]",
-                    "px-6 py-2.5",
-                    "text-sm font-medium",
-                    "transition-all duration-300",
-                    "md:gap-2",
-                    "hover:text-zinc-100"
-                  )}
-                >
-                  {isActive && (
-                    <motion.div
-                      layoutId="active-pill"
-                      transition={{
-                        type: "spring",
-                        stiffness: 350,
-                        damping: 30,
-                      }}
-                      className="
+                      setActive(item.href);
+                    }}
+                    onMouseEnter={() => {
+                      if (item.hasDropdown && !moreOpen) {
+                        setMoreOpen(true);
+                      }
+                    }}
+                    className={cn(
+                      "relative flex items-center justify-center rounded-[30px]",
+                      "px-6 py-2.5",
+                      "text-sm font-medium",
+                      "transition-all duration-300",
+                      "md:gap-2",
+                      "hover:text-zinc-100"
+                    )}
+                  >
+                    {isActive && (
+                      <motion.div
+                        layoutId="active-pill"
+                        transition={{
+                          type: "spring",
+                          stiffness: 350,
+                          damping: 30,
+                        }}
+                        className="
                         absolute inset-0 rounded-full
                         border border-white/10
                         bg-linear-to-b
@@ -106,41 +110,41 @@ export function NavBar({ items, className }: NavBarProps) {
                         to-zinc-700/40
                         shadow-[inset_0_1px_1px_rgba(255,255,255,0.08),0_0_12px_rgba(255,255,255,0.08)]
                       "
-                    />
-                  )}
-
-                  <span
-                    className={cn(
-                      "relative z-10 hidden md:block",
-                      "hover:text-zinc-100",
-                      "transition-colors duration-300",
-                      // Active state takes priority
-                      isActive ? "text-white" : "text-zinc-400 "
-                    )}
-                  >
-                    {item.name}
-                  </span>
-                  {item.hasDropdown && (
-                    <motion.div
-                      animate={{
-                        rotate: moreOpen ? 180 : 0,
-                      }}
-                    >
-                      <CaretDown
-                        size={14}
-                        className="relative z-10 text-zinc-400"
                       />
-                    </motion.div>
-                  )}
-                  {isActive && (
-                    <motion.div
-                      layoutId="glow"
-                      className="absolute -top-1 left-1/2 h-1.5 w-8 -translate-x-1/2 rounded-full bg-zinc-200 opacity-70"
-                    />
-                  )}
-                </a>
-              );
-            })}
+                    )}
+
+                    <span
+                      className={cn(
+                        "relative z-10 hidden md:block",
+                        "hover:text-zinc-100",
+                        "transition-colors duration-300",
+                        // Active state takes priority
+                        isActive ? "text-white" : "text-zinc-400 "
+                      )}
+                    >
+                      {item.name}
+                    </span>
+                    {item.hasDropdown && (
+                      <motion.div
+                        animate={{
+                          rotate: moreOpen ? 180 : 0,
+                        }}
+                      >
+                        <CaretDown
+                          size={14}
+                          className="relative z-10 text-zinc-400"
+                        />
+                      </motion.div>
+                    )}
+                    {isActive && (
+                      <motion.div
+                        layoutId="glow"
+                        className="absolute -top-1 left-1/2 h-1.5 w-8 -translate-x-1/2 rounded-full bg-zinc-200 opacity-70"
+                      />
+                    )}
+                  </Link>
+                );
+              })}
           </div>
 
           {/* Expandable Area */}
@@ -160,8 +164,8 @@ export function NavBar({ items, className }: NavBarProps) {
         </motion.div>
 
         {/* Right Button */}
-        <a
-          href="#contact"
+        <Link
+          to="#contact"
           className="
             justify-self-end
             rounded-full
@@ -176,7 +180,84 @@ export function NavBar({ items, className }: NavBarProps) {
           "
         >
           Let's Talk
-        </a>
+        </Link>
+      </div>
+      <div className="flex justify-center md:hidden">
+        <Drawer>
+          <DrawerTrigger asChild>
+            <button
+              className="
+                flex
+                w-3/5
+                max-w-sm
+                items-center
+                justify-around
+                gap-3
+                rounded-full
+                border
+                border-white/10
+                bg-zinc-900/70
+                
+                backdrop-blur-2xl
+              "
+            >
+              <img
+                src="/logo-white.png"
+                alt="Logo"
+                className="h-10 w-10"
+              />
+
+              <span className="font-medium text-white uppercase">
+                Kalyan
+              </span>
+            </button>
+          </DrawerTrigger>
+
+          <DrawerContent
+            className="
+            max-h-[80vh]
+            max-w-sm
+            overflow-y-auto
+            rounded-3xl
+            border-white/10
+            bg-zinc-900/90
+            backdrop-blur-3xl
+          "
+          >
+            <div className="space-y-4">
+
+              <h2 className="text-center text-xl font-semibold text-white">
+                Navigation
+              </h2>
+
+              <div className="grid grid-cols-2 gap-3">
+
+                {items
+                .filter((item) => !item.hasDropdown)
+                .map((item) => (
+                  <Link
+                    key={item.href}
+                    to={item.href}
+                    className="
+                      rounded-xl
+                      border border-white/10
+                      bg-white/5
+                      p-4
+                      text-center
+                      text-zinc-300
+                      transition
+                      hover:bg-white/10
+                    "
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+
+              </div>
+
+            </div>
+          </DrawerContent>
+        </Drawer>
       </div>
     </header>
   );
